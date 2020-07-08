95baf89d36db969a081ed19b1d9d25e7c751500b
Floui2017 <315587951@qq.com>
2020-07-08 17:56:04 +0800

    first commit

---
+++ src/main/java/com/flouis/jdsa/JavaDataStructureAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.flouis.jdsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDataStructureAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDataStructureAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flouis/jdsa/base/TimeComplexityTests.java
@@ -0,0 +1,14 @@
+package com.flouis.jdsa.base;
+
+import org.junit.Test;
+
+public class TimeComplexityTests {
+
+	@Test
+	public void mainTest(){
+		System.out.println(Thread.currentThread().getName());
+	}
+
+
+
+}
